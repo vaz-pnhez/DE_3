7432da187792fd5403246dfe50416d8f0fd164fb
LSNr-tqn <tranquangnghia260806@gmail.com>
2025-11-17 20:50:57 +0700

    full 1.0

--- DSA_BTL.c
+++ DSA_BTL.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+typedef struct tagSN {
+    int data;
+} SN;
 
 typedef struct tagDNode {
-    int data;
+    SN *value;
     struct tagDNode *Pre, *Next;
 } DNode;
 
@@ -16,10 +21,12 @@ void CreateDList(DList *l) {
     l->Tail = NULL;
 }
 
-DNode* CreateNode(int x) {
-    DNode *n = (DNode*) malloc(sizeof(DNode));
-    if(!n) exit(1);
-    n->data = x;
+DNode *CreateNode(int x) {
+    SN *num = (SN*) malloc(sizeof(SN));
+    num->data = x;
+    DNode *n = (DNode *)malloc(sizeof(DNode));
+    if (!n || !num) exit(1);
+    n->value = num;
     n->Pre = NULL;
     n->Next = NULL;
     return n;
@@ -35,14 +42,14 @@ void AddNode(DList *l) {
         if (l->Head == NULL) {
             l->Head = n;
             l->Tail = l->Head;
-        } else {
+        }
+        else {
             l->Tail->Next = n;
             n->Pre = l->Tail;
             l->Tail = n;
         }
     }
 }
-
 
 int isPrime(int n) {
     if (n < 2) return 0;
@@ -52,63 +59,90 @@ int isPrime(int n) {
     return 1;
 }
 
-void DeleteAtPosition(DList *l, int k) {
-    if (k <= 0 || l->Head == NULL) return;
-    DNode *n = l->Head;
-    int count = 1;
-    while (n && count < k) {
+DNode *findLocation(DList l, int locate) {
+    DNode *n = l.Head;
+    int i = 1;
+    while (n && i < locate) {
         n = n->Next;
-        count++;
+        i++;
     }
-    if (n == NULL) return;
-
-    if (n == l->Head) l->Head = n->Next;
-    if (n == l->Tail) l->Tail = n->Pre;
-    if (n->Pre) n->Pre->Next = n->Next;
-    if (n->Next) n->Next->Pre = n->Pre;
-
-    printf("Da xoa phan tu o vi tri %d co gia tri %d\n", k, n->data);
-    free(n);
+    return n;
 }
 
+void DeleteNode(DList *l, DNode *n){
+    if (n == l->Head)
+        l->Head = n->Next;
+    if (n == l->Tail)
+        l->Tail = n->Pre;
+    if (n->Pre)
+        n->Pre->Next = n->Next;
+    if (n->Next)
+        n->Next->Pre = n->Pre;
+}
 
+void DeleteAtPosition(DList *l, int locate) {
+    DNode *n = findLocation(*l, locate);
+    if (!n) {
+        printf("Vuot qua so luong phan tu List hoac List rong!!");
+        return; 
+    }
+    if (isPrime(n->value->data)) {
+        printf("Da xoa phan tu o vi tri %d co gia tri %d\n", locate, n->value->data);
+        DeleteNode(l, n);
+        free(n->value);
+        free(n);
+    } else printf("Phan tu o vi tri %d khong phai so nguyen to!!\n", locate);
+}
 
 int PowofNum(DList l, int x) {
     DNode *n = l.Head;
     long long powN = 1;
-    while(n) {
-        if (n->data > 0 && n->data % x == 0 && n->data % 2 == 0) powN *= n->data;
+    while (n) {
+        if (n->value->data > 0 && n->value->data % x == 0 && n->value->data % 2 == 0)
+            powN *= n->value->data;
         n = n->Next;
     }
     return powN;
 }
 
 void SortList(DList *l) {
-    
+    DNode *i = l->Head;
+    if (!i) return;
+    for (i; i != NULL; i = i->Next) {
+        for (DNode *j = l->Tail; j != i; j = j->Pre) {
+            if (j->value->data < j->Pre->value->data) {
+                SN *temp = j->value;
+                j->value = j->Pre->value;
+                j->Pre->value = temp;
+            }
+        }
+    }
 }
 
 void FilterList(DList *l) {
-    for (DNode *n = l->Head; n ; n = n->Next) {
+    for (DNode *n = l->Head; n; n = n->Next) {
         DNode *p = n->Next;
-        while(p) {
-            if (p->data == n->data) { // Check data(p) = data(n) ?
+        while (p) {
+            if (p->value->data == n->value->data) { // Check data(p) = data(n) ?
                 DNode *temp = p;
                 p = p->Next;
-
                 // Check p la Head ?
                 if (temp == l->Head) {
                     l->Head = temp->Next;
-                } else {
+                }
+                else {
                     temp->Pre->Next = temp->Next;
                 }
                 // Check p la Tail ?
                 if (temp == l->Tail) {
                     l->Tail = temp->Pre;
-                } else {
+                }
+                else {
                     temp->Next->Pre = temp->Pre;
                 }
                 free(temp);
-            } else p = p->Next;
+            }
+            else p = p->Next;
         }
     }
 }
@@ -116,7 +150,7 @@ void FilterList(DList *l) {
 void printList(DList l) {
     DNode *n = l.Head;
     while (n) {
-        printf("%d ", n->data);
+        printf("%d ", n->value->data);
         n = n->Next;
     }
     printf("\n");
@@ -126,7 +160,7 @@ void printAddvsData(DList l) {
     DNode *n = l.Head;
     int count = 1;
     while (n) {
-        printf("%3d | %15d | %10d\n", count, n, n->data);
+        printf("%3d | %15d | %10d\n", count, n, n->value->data);
         n = n->Next;
         count++;
     }
@@ -147,52 +181,42 @@ int main() {
         printf("7. Hien thi Fibonaci.\n");
         printf("0. Thoat.\n");
         printf("\nChoice: ");
-        int opt; 
-        scanf("%d", &opt);    
-        switch(opt) {
-            case 1: 
-                AddNode(&l);
-                printList(l);
-                break;
-            case 2: 
-    int pos = 5;
-    DNode *n = l.Head;
-    int count = 1;
-    while (n && count < pos) {
-        n = n->Next;
-        count++;
-    }
-    if (n == NULL) {
-        printf("Danh sach khong co du 5 phan tu!\n");
-        break;
-    }
-    if (isPrime(n->data)) {
-        printf("Phan tu o vi tri thu 5 = %d la so nguyen to -> Xoa no!\n", n->data);
-        DeleteAtPosition(&l, pos);
-    } else {
-        printf("Phan tu o vi tri thu 5 = %d KHONG phai so nguyen to.\n", n->data);
-    }
-    printList(l);
-    break;
-
-            case 3:
-                if (PowofNum(l, 5) > 1) printf("Tich cua cac so chan, duong chia het cho 5: %d\n",  PowofNum(l, 5));
-                else printf("Khong co so nao thoa man!!\n");
-                break;
-            case 4:
-                break;
-            case 5: 
-                FilterList(&l); 
-                printf("Thanh cong!\n");
-                break;
-            case 6:
-                printf("---- Danh sach phan tu ----\n");
-                printf("%3s | %15s | %10s\n", "STT", "Address", "Data");
-                printAddvsData(l); 
-                break;
-            case 7: break;
-            case 0: printf("Dang thoat ...."); return 0;
-            default: printf("Lua chon khong hop le!!");
+        int opt;
+        scanf("%d", &opt);
+        switch (opt) {
+        case 1:
+            AddNode(&l);
+            printList(l);
+            break;
+        case 2:
+            DeleteAtPosition(&l,5);
+            printList(l);
+            break;
+        case 3:
+            if (PowofNum(l, 5) > 1)
+                printf("Tich cua cac so chan, duong chia het cho 5: %d\n", PowofNum(l, 5));
+            else
+                printf("Khong co so nao thoa man!!\n");
+            break;
+        case 4:
+            SortList(&l);
+            break;
+        case 5:
+            FilterList(&l);
+            printf("Thanh cong!\n");
+            break;
+        case 6:
+            printf("---- Danh sach phan tu ----\n");
+            printf("%3s | %15s | %10s\n", "STT", "Address", "Data");
+            printAddvsData(l);
+            break;
+        case 7:
+            break;
+        case 0:
+            printf("Dang thoat ....");
+            return 0;
+        default:
+            printf("Lua chon khong hop le!!");
         }
     }
 }
